f6807159b99889e729415a221e16aeeb583be288
goupgoup <a.catacutan.549237@umindanao.edu.ph>
2025-12-11 07:14:04 +0800

    v1.0

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Url;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if($this->app->environment('production')) {
-            Url::forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
